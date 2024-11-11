184cacf85bd553db6af9930de3f05b25c3c90d7d
Ali Hussein <ali.hussein@live.com>
2024-11-11 03:50:44 +0200

    Header navigation created for Mobile and Desktop

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild('mobileMenu') menu!: ElementRef;
+
+  open() {
+    this.menu.nativeElement.classList.add('open');
+  }
+  close() {
+    this.menu.nativeElement.classList.remove('open');
+  }
 
 }
